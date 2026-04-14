1f6c242f059343549d54aa1c57f64c8e3d41baa4
Ahoo Wang <ahoowang@qq.com>
2026-04-14 13:45:51 +0800

    feat(core): add optional reactor and coroutines support features
    - Register reactorSupport and coroutinesSupport as optional features
    - Move coroutines dependency to coroutinesSupportImplementation scope
    - Move reactor dependency to reactorSupportImplementation scope
    - Enable capability-based dependency management for reactive libraries
    - Allow consumers to opt-in to specific reactive programming support

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,17 @@
+java {
+    registerFeature("reactorSupport") {
+        usingSourceSet(sourceSets[SourceSet.MAIN_SOURCE_SET_NAME])
+        capability(group.toString(), "reactor-support", version.toString())
+    }
+    registerFeature("coroutinesSupport") {
+        usingSourceSet(sourceSets[SourceSet.MAIN_SOURCE_SET_NAME])
+        capability(group.toString(), "coroutines-support", version.toString())
+    }
+}
+
 dependencies {
     api(libs.jackson.annotations)
-    implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.reactor.core)
+    "coroutinesSupportImplementation"(libs.kotlinx.coroutines.core)
+    "reactorSupportImplementation"(libs.reactor.core)
     testImplementation(libs.reactor.test)
 }
